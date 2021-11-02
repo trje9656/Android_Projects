f56cc022b2baf9fa6d883d05fe14d91d66e6472f
trevjed <trevjed@gmail.com>
2021-11-01 22:35:29 -0600

    Added edit Text functionality, constraints, custom background color

--- PuncherApp/app/src/main/java/com/example/puncherapp/MainActivity.kt
+++ PuncherApp/app/src/main/java/com/example/puncherapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.puncherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+fun punchName(view: View){
+    val editName = findViewById<EditText>(R.id.editTextFace)
+    val name = editName.text
+
+    val punchText = findViewById<TextView>(R.id.punch_msg)
+    punchText.text = "Take That " + name + "!"
+
+}
+
 }
