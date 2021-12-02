2b5d2498ccb85f28d08185c7b6c462234d599fa6
Trevor Jedziniak <trje9656@colorado.edu>
2021-12-01 21:59:33 -0700

    setup activity layouts

--- Lab8/app/src/main/java/com/example/lab8/MainActivity.kt
+++ Lab8/app/src/main/java/com/example/lab8/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.lab8
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        /*locationButton.setOnClickListener {
+            selectedLocationPosition = spinner.selectedItemPosition
+            myTacoShop.suggestTacoShop(selectedLocationPosition)
+            Log.i("shop suggested", myTacoShop.name);
+            Log.i("url suggested", myTacoShop.url);
+
+            //create intent
+            val intent = Intent(this, TacoActivity::class.java)
+            intent.putExtra("tacoShopName", myTacoShop.name)
+            intent.putExtra("tacoShopURL", myTacoShop.url)
+
+            startActivityForResult(intent, REQUEST_CODE)
+        }*/
+
+
     }
 }
