8c51cd6fe32ad9301d0d86b77f1425f7df178137
trevjed <trevjed@gmail.com>
2021-12-02 15:07:03 -0700

    added explicit intent and changed gradle config due to error

--- Lab8/app/src/main/java/com/example/lab8/MainActivity.kt
+++ Lab8/app/src/main/java/com/example/lab8/MainActivity.kt
@@ -1,29 +1,38 @@
 package com.example.lab8
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE = 1
+    lateinit var pageswitch : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pageswitch = findViewById(R.id.pageswitch)
 
 
-        /*locationButton.setOnClickListener {
-            selectedLocationPosition = spinner.selectedItemPosition
+        pageswitch.setOnClickListener {
+           /* selectedLocationPosition = spinner.selectedItemPosition
             myTacoShop.suggestTacoShop(selectedLocationPosition)
             Log.i("shop suggested", myTacoShop.name);
-            Log.i("url suggested", myTacoShop.url);
+            Log.i("url suggested", myTacoShop.url);*/
 
             //create intent
-            val intent = Intent(this, TacoActivity::class.java)
-            intent.putExtra("tacoShopName", myTacoShop.name)
-            intent.putExtra("tacoShopURL", myTacoShop.url)
+            val intent = Intent(this, MainActivity2::class.java)
+            /*intent.putExtra("tacoShopName", myTacoShop.name)
+            intent.putExtra("tacoShopURL", myTacoShop.url)*/
 
             startActivityForResult(intent, REQUEST_CODE)
-        }*/
+        }
 
 
     }
