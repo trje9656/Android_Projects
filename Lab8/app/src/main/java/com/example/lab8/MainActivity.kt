4618d6907283c6d193a540db0014a3dd79a68762
trevjed <trevjed@gmail.com>
2021-12-02 16:53:42 -0700

    missing commit

--- Lab8/app/src/main/java/com/example/lab8/MainActivity.kt
+++ Lab8/app/src/main/java/com/example/lab8/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lab8
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,30 +11,55 @@ import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.switchmaterial.SwitchMaterial
-
+//https://developer.android.com/training/basics/intents/sending
 class MainActivity : AppCompatActivity() {
     private val REQUEST_CODE = 1
     lateinit var pageswitch : Button
+    lateinit var blackjack : Button
+    lateinit var EightballButton : Button
+    lateinit var coinButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pageswitch = findViewById(R.id.pageswitch)
+        blackjack = findViewById(R.id.BlackjackButton)
+        EightballButton = findViewById(R.id.EightballButton)
+        coinButton = findViewById(R.id.coinButton)
+
+
 
 
         pageswitch.setOnClickListener {
-           /* selectedLocationPosition = spinner.selectedItemPosition
-            myTacoShop.suggestTacoShop(selectedLocationPosition)
-            Log.i("shop suggested", myTacoShop.name);
-            Log.i("url suggested", myTacoShop.url);*/
+
 
             //create intent
             val intent = Intent(this, MainActivity2::class.java)
-            /*intent.putExtra("tacoShopName", myTacoShop.name)
-            intent.putExtra("tacoShopURL", myTacoShop.url)*/
+
 
             startActivityForResult(intent, REQUEST_CODE)
         }
 
 
+    }
+    fun blackjackSwitch(view: View){
+        val webIntent: Intent = Uri.parse("https://html5blackjack.net/").let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(webIntent)
+
+    }
+
+    fun coinSwitch(view: View){
+        val webIntent: Intent = Uri.parse("https://flipsimu.com/").let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(webIntent)
+    }
+
+    fun EightBallSwitch(view: View){
+        val webIntent: Intent = Uri.parse("http://www.ask8ball.net/").let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(webIntent)
     }
 }
--- Lab8/app/src/main/java/com/example/lab8/MainActivity2.kt
+++ Lab8/app/src/main/java/com/example/lab8/MainActivity2.kt
@@ -1,11 +1,38 @@
 package com.example.lab8
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var BackstoryButton : Button
+    lateinit var RPGButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        RPGButton = findViewById(R.id.RPGButton)
+        BackstoryButton = findViewById(R.id.BackstoryButton)
+
+
+    }
+
+    fun BackstorySwitch(view: View){
+        val webIntent: Intent = Uri.parse("https://en.shindanmaker.com/519584").let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(webIntent)
+
+    }
+
+    fun RPGSwitch(view: View){
+        val webIntent: Intent = Uri.parse("https://en.shindanmaker.com/645752").let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(webIntent)
     }
 }
